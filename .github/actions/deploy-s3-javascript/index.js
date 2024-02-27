73881a5dcda7488049787a4683f1b072cbbd2b01
gavin.xx@nodereal.io <gavin.xx@nodereal.io>
2024-02-27 18:18:13 +0800

    uploading to s3

--- .github/actions/deploy-s3-javascript/index.js
+++ .github/actions/deploy-s3-javascript/index.js
@@ -3,7 +3,16 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
+    //get all inputs
+    const bucketName = core.getInput('bucket-name', { required: true })
+    const bucketRegion = core.getInput('bucket-region', { required: true })
+    const folder = core.getInput('dist-folder', { required: true })
+
+    //upload files
+    exec.exec(`echo "uploading ${folder} to ${bucketRegion}/${bucketName}" ...`)
+
     core.notice('hello from my javascript action !!!')
+
 }
 
 run();
